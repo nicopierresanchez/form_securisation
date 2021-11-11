735df8c4a568d60aa4acc19d32c1339086d2d6ef
SANCHEZ nicolas <77881361+nicopierresanchez@users.noreply.github.com>
2021-11-11 12:55:45 +0100

    first

--- public/thanks.php
+++ public/thanks.php
@@ -1,21 +1,23 @@
 <?php
 
 
-$userNameError = "Un nom valide est requis !";
+$usermail = $_POST["user_mail"];
 
-$userFirstNameError = "Un prénom valide est requis !";
+$userNameError = "Un nom est requis !";
+
+$userFirstNameError = "Un prénom est requis !";
 
 $usermailError = "Un E-mail est requis";
 
-$téléphoneError = "Un numéro de téléphone valide est requis !";
+$téléphoneError = "Un numéro de téléphone est requis !";
 
-$sportError = "Vous devez spécifier un sujet";
+$sportError = "Vous devez choisir un sport";
 
 $userMessageError = "Un message est requis";
 
 
 $message = "Merci " . $_POST['user_firstname'] . " " . $_POST['user_name'] . " de nous avoir contacté à propos de " . $_POST['sport'] . " ." . PHP_EOL . "Un de nos conseiller vous contactera soit à l'adresse " . $_POST['user_mail'] . " ou par téléphone au " . $_POST['téléphone'] . " dans les plus brefs délais pour traiter votre demande :" . PHP_EOL . $_POST['user_message'];
-$usermail = $_POST["user_mail"];
+
 
 
 
